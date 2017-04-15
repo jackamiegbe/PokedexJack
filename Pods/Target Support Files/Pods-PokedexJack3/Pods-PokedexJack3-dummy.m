850aabf284d575213de96a37090932ff91522c95
git@github.com:jackamiegbe/jackattack.git <geeksgofar@gmail.com>
2017-04-15 18:04:00 +0100

    music

---
+++ Pods/Target Support Files/Pods-PokedexJack3/Pods-PokedexJack3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PokedexJack3 : NSObject
+@end
+@implementation PodsDummy_Pods_PokedexJack3
+@end
---
+++ PokedexJack3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  PokedexJack3
+//
+//  Created by MACBOOK on 10/04/2017.
+//  Copyright © 2017 jack_amiegbe. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
